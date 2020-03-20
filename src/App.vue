0952e529eefa74ef083a74cfb98396ff77bf6f89
myrnf <mayur@newfang.io>
2020-03-20 16:19:50 +0530

    checking document referrer

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
   mounted() {
     this.sender = this.$route.query.sender;
     this.count = this.$route.query.count;
-    console.log(document.referrer);
+    console.log('docref', document.referrer);
   }
 }
 </script>
